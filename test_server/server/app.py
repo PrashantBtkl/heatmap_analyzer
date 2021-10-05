05ee362484a02fa25518fffbc385785cbad77ee8
PrashantBtkl <prashant@kawa.space>
2021-10-05 20:12:54 +0530

    store to database

--- test_server/server/app.py
+++ test_server/server/app.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 import asyncio
 import websockets
+import json
+import db
 
-async def server(websocket, path):
+
+def store(pos):
+	conn = db.get_conn()
+
+	pos = json.loads(pos,)
+	try:
+		db.store_positions(pos, conn)
+		print("successfully stored for page_id",pos["page_id"])
+	except Exception as e:
+		print("exception occured :", e)
+	finally:
+		conn.close()
+
+
+
+async def get_info(websocket, path):
 	while True:
 		data = await websocket.recv()
-		print(f"< {data}")
+		if data:
+			store(data)
 
 
-start_server = websockets.serve(server, "localhost", 8765)
+start_server = websockets.serve(get_info, "localhost", 8765)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
