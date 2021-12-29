65b25e180853b830b17d7d6b025d7100cec61223
PrashantBtkl <prashant44backup@gmail.com>
2021-12-29 18:03:26 +0530

    modified tracker.js and index.html file

--- test_server/client/js/tracker.js
+++ test_server/client/js/tracker.js
@@ -1,4 +1,13 @@
 var ws = new WebSocket("ws://localhost:8765");
+const ses_id = uuidv4();
+const pg_id = "694da4d3-56c1-4ce5-a8e9-e5ab7462a78b";
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+};
+
 (function() {
     "use strict";
 
@@ -10,7 +19,7 @@ var ws = new WebSocket("ws://localhost:8765");
     function initWatchers() {
         // watch mouse clicks
         watch(document, 'click', function(event) {
-            return { type: 'MOUSE_CLICK', x: event.clientX, y: event.clientY };
+            return { session_id: ses_id, page_id: pg_id, x: event.clientX, y: event.clientY };
         });
 
         // watch mouse move
@@ -30,7 +39,6 @@ var ws = new WebSocket("ws://localhost:8765");
     }
 
     function handleEvent(event) {
-        event.ts = new Date().getTime();
 		ws.send(JSON.stringify(event));
         console.log(event);
     }
